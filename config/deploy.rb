4f15f76a2d5ad64a363b33630d3cca6614e02c6e
thelowlypeon <me@petercompernolle.com>
2017-04-02 07:59:31 -0500

    fixes remote repo path

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "simple_logger"
-set :repo_url, "git@github.com:thelowlypeon/simple_logger.git"
+set :repo_url, "git@github.com:thelowlypeon/simple_logger_server.git"
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
